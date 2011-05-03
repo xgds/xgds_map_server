4daa4213144d14835721d4005b7b94e8efd9562f
Trey Smith <trey.smith@nasa.gov>
2011-05-03 11:31:26 -0700

    shortened urls based on how they are included in site

--- xgds_map_server/urls.py
+++ xgds_map_server/urls.py
@@ -9,17 +9,17 @@ from django.conf.urls.defaults import *
 from xgds_map_server import settings
 from xgds_map_server.views import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
+    (r'^$', getMapListPage),
     # Map server urls
     # HTML list of maps with description and links to individual maps, and a link to the kml feed
-    (r'^mapserver/list/', getMapListPage),
-    # This URL should retrieve a top-level KML file with network link to the top-level feed
-    #(r'^xgdsPlrp/mapserver/feed', getMapFeed,{'feedname':''}),
+    (r'^list/', getMapListPage),
     # This URL should receive a static files
-    (r'^data/mapserver/(?P<path>.*)$', 'django.views.static.serve',
+    (r'^data/(?P<path>.*)$', 'django.views.static.serve',
      {'document_root' : settings.MAPSERVER_FILE_ROOT,
       'show_indexes' : True}),
     # By default if you just load the app you should see the list
-    (r'^mapserver/feed/(?P<feedname>.*)', getMapFeed),
-    (r'^mapserver', getMapListPage),
+    (r'^feed/(?P<feedname>.*)', getMapFeed),
 )
