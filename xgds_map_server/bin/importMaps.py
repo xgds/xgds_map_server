83b8fd54e31d0d9370e9431468a565c7555b4604
Trey Smith <trey.smith@nasa.gov>
2011-08-26 16:51:00 -0700

    fixed importMaps

--- xgds_map_server/bin/importMaps.py
+++ xgds_map_server/bin/importMaps.py
@@ -3,16 +3,23 @@
 import sys
 import os
 import stat
+import re
 
 from xgds_map_server.models import Map, MapGroup
 from xgds_map_server import settings
 
 def getName(path):
-    return re.sub('_', ' ', os.path.basename(path))
+    path = os.path.basename(path)
+    path = os.path.splitext(path)[0]
+    path = re.sub('_', ' ', path)
+    return path
 
 def importMaps(parent, paths):
     if isinstance(parent, str):
         parent = MapGroup.objects.get(parent)
+    elif parent == None:
+        print 'no parent specified, fetching the root MapGroup to use as the parent'
+        parent = MapGroup.objects.get(parentId=None)
     mapDir = os.path.realpath(settings.DATA_ROOT + settings.XGDS_MAP_SERVER_DATA_SUBDIR)
     cwd = os.path.realpath(os.getcwd())
     if mapDir != cwd:
@@ -32,9 +39,14 @@ def importMaps(parent, paths):
                     print 'add    MapGroup %s -- parent %s' % (name, parent.name)
                 else:
                     print 'exists MapGroup %s' % name
-                for sub in os.listdir(path):
-                    importMaps(group, os.path.join(path, sub))
+                importMaps(group, [os.path.join(path, sub)
+                                   for sub in os.listdir(path)
+                                   if not sub.startswith('.')])
             elif os.path.isfile(path):
+                ext = os.path.splitext(path)[1]
+                if ext not in ('.kml', '.kmz'):
+                    print 'file %s is not a map, skipping' % path
+                    continue
                 map, created = (Map.objects.get_or_create
                                 (name=name,
                                  defaults=dict(description='-',
