3676baa835dc53935afafbb5fcbeaf081f9744cf
Trey Smith <trey.smith@nasa.gov>
2013-10-14 18:53:47 -0700

    fix lint warnings in JS files

--- xgds_map_server/static/xgds_map_server/js/MapForm.js
+++ xgds_map_server/static/xgds_map_server/js/MapForm.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     function toggleKMLocalFile() {
-	if ($("input[name='typeChooser'][value='kml']").is(":checked")) {
-	    $("#kmlFile_div").show();
-	    $("#id_localFile").val('');
-	    $("#localFile_div").hide();
-	} else if ($("input[name='typeChooser'][value='file']").is(":checked")) {
-	    $("#kmlFile_div").hide();
-	    $("#localFile_div").show();
-	}
+        if ($("input[name='typeChooser'][value='kml']").is(':checked')) {
+            $('#kmlFile_div').show();
+            $('#id_localFile').val('');
+            $('#localFile_div').hide();
+        } else if ($("input[name='typeChooser'][value='file']").is(':checked')) {
+            $('#kmlFile_div').hide();
+            $('#localFile_div').show();
+        }
     }
 
     $("input[name='typeChooser']").change(toggleKMLocalFile);
--- xgds_map_server/static/xgds_map_server/js/MapTree.js
+++ xgds_map_server/static/xgds_map_server/js/MapTree.js
@@ -1,54 +1,54 @@
-$(document).ready(function(){
+$(document).ready(function() {
     function process_move(node, ref, position,
-			 is_copy, is_prepared, skip_check) {
-	if (skip_check) return; // we don't want to do moves that aren't checked
-	var tree = $.jstree._reference("treeDiv");
-	var move = tree._get_move();
-	$.get(jsMoveURL, {move:move.o.data("id"),
-			   move_type:move.o.data("type"),
-			   to:move.np.data("id"),
-			   to_type:move.np.data("type")})
-	    .success(function () {
-		console.log("Move succeeded");
-		tree.refresh();
-	    }).fail(function () {
-		alert("Move failed");
-		tree.refresh();
-	    });
+                         is_copy, is_prepared, skip_check) {
+        if (skip_check) return; // we don't want to do moves that aren't checked
+        var tree = $.jstree._reference('treeDiv');
+        var move = tree._get_move();
+        $.get(jsMoveURL, {move: move.o.data('id'),
+                          move_type: move.o.data('type'),
+                          to: move.np.data('id'),
+                          to_type: move.np.data('type')})
+            .success(function() {
+                console.log('Move succeeded');
+                tree.refresh();
+            }).fail(function() {
+                alert('Move failed');
+                tree.refresh();
+            });
     }
-    $("#treeDiv")
-    .bind("move_node.jstree", process_move)
+    $('#treeDiv')
+    .bind('move_node.jstree', process_move)
     .jstree({
-	"json_data": {
-	    "ajax": {
-		"url": jsTreeMapTreeURL
-	    },
-	    "progressive_render": true,
-	    "progressive_unload": false
-	},
-	"themes": {
-	    "theme": "default",
-	    "dots": true,
-	    "icons": true,
-	},
-	"dnd": {
-	    "drag_target": false,
-	    "drop_target": false,
-	},
-	"crrm": {
-	    "move": {
-		"check_move": function(move) {
-		    var parent = this._get_parent(move.o);
-		    if (!parent) return false;
-		    parent = parent == -1 ? this.get_container() : parent;
-		    if (parent === move.np) return false;
-		    if (parent[0] && move.np[0] && parent[0] === move.np[0])
-			return false;
-		    if (move.np.data("type") != "folder") return false;
-		    return true;
-		}
-	    },
-	},
-	"plugins": ["themes", "json_data", "dnd", "crrm"]
+        'json_data': {
+            'ajax': {
+                'url': jsTreeMapTreeURL
+            },
+            'progressive_render': true,
+            'progressive_unload': false
+        },
+        'themes': {
+            'theme': 'default',
+            'dots': true,
+            'icons': true
+        },
+        'dnd': {
+            'drag_target': false,
+            'drop_target': false
+        },
+        'crrm': {
+            'move': {
+                'check_move': function(move) {
+                    var parent = this._get_parent(move.o);
+                    if (!parent) return false;
+                    parent = parent == -1 ? this.get_container() : parent;
+                    if (parent === move.np) return false;
+                    if (parent[0] && move.np[0] && parent[0] === move.np[0])
+                        return false;
+                    if (move.np.data('type') != 'folder') return false;
+                    return true;
+                }
+            }
+        },
+        'plugins': ['themes', 'json_data', 'dnd', 'crrm']
     });
 });
