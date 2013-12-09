7643eadaed43a7932112671fa13d2cd3f0c9eb76
Jerome Rasky <jyrome.112@gmail.com>
2013-12-08 20:47:47 -0800

    Created fixtures for tests, enabled more tests

--- xgds_map_server/tests.py
+++ xgds_map_server/tests.py
@@ -6,14 +6,15 @@
 
 # import logging
 
+#TODO: write tests that do more than check pages with GET requests.
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 
 class TestMaps(TestCase):
-    """
-    Tests for xgds_map_server
-    """
+    fixtures = ['xgds_map_server_testing.json',
+                'xgds_map_server_auth.json']
 
     def test_index(self):
         response = self.client.get(reverse('xgds_map_server_index'))
@@ -31,61 +32,51 @@ class TestMaps(TestCase):
         response = self.client.get(reverse('mapTree'))
         self.assertEqual(response.status_code, 200)
 
-    # TODO needs a fixture (assumes there is a root group)
-    # def test_mapListJSON(self):
-    #     response = self.client.get(reverse('mapListJSON'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_mapListJSON(self):
+        response = self.client.get(reverse('mapListJSON'))
+        self.assertEqual(response.status_code, 200)
 
-    # TODO needs fixture
-    # def test_mapDetail(self):
-    #     mapId = '...'
-    #     response = self.client.get(reverse('mapDetail', args=[mapId]))
-    #     self.assertEqual(response.status_code, 200)
+    def test_mapDetail(self):
+        mapId = '1'
+        response = self.client.get(reverse('mapDetail', args=[mapId]))
+        self.assertEqual(response.status_code, 200)
 
-    # TODO needs fixture
-    # def test_folderDetail(self):
-    #     groupId = '...'
-    #     response = self.client.get(reverse('folderDetail', args=[groupId]))
-    #     self.assertEqual(response.status_code, 200)
+    def test_folderDetail(self):
+        groupId = '1'
+        response = self.client.get(reverse('folderDetail', args=[groupId]))
+        self.assertEqual(response.status_code, 200)
 
-    # TODO needs fixture
-    # def test_folderDelete(self):
-    #     groupId = '...'
-    #     response = self.client.get(reverse('folderDelete', args=[groupId]))
-    #     self.assertEqual(response.status_code, 200)
+    def test_folderDelete(self):
+        groupId = '1'
+        response = self.client.get(reverse('folderDelete', args=[groupId]))
+        self.assertEqual(response.status_code, 200)
 
-    # TODO figure out post args
-    # def test_folderAdd(self):
-    #     response = self.client.get(reverse('folderAdd'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_folderAdd(self):
+        response = self.client.get(reverse('folderAdd'))
+        self.assertEqual(response.status_code, 200)
 
-    # FIX do a post
-    # def test_addMap(self):
-    #     response = self.client.get(reverse('addMap'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_addMap(self):
+        response = self.client.get(reverse('addMap'))
+        self.assertEqual(response.status_code, 200)
 
-    # FIX do a post
-    # def test_mapDelete(self):
-    #     response = self.client.get(reverse('mapDelete'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_mapDelete(self):
+        mapId = '1'
+        response = self.client.get(reverse('mapDelete', args=[mapId]))
+        self.assertEqual(response.status_code, 200)
 
     def test_deletedMaps(self):
         response = self.client.get(reverse('deletedMaps'))
         self.assertEqual(response.status_code, 200)
 
-    # FIX do a post
     # def test_jsonMove(self):
     #     response = self.client.get(reverse('jsonMove'))
     #     self.assertEqual(response.status_code, 200)
 
-    # FIX test fails... is this view used anyway?
-    # def test_static(self):
-    #     logging.info('test_static: %s', reverse('xgds_map_server_static', args=['']))
-    #     response = self.client.get(reverse('xgds_map_server_static', args=['']))
-    #     self.assertEqual(response.status_code, 200)
+    # The xgds_map_server_static function was a hack I think I (Jerome) created because
+    # I didn't know how to work the static file settings. Indeed it isn't used.
 
-    # TODO needs fixture
-    # def test_feed(self):
-    #     feedName = '...'
-    #     response = self.client.get(reverse('xgds_map_server_feed', args=[feedName]))
-    #     self.assertEqual(response.status_code, 200)
+    def test_feed(self):
+        # currently the only feed possible is the master feed
+        feedName = ''
+        response = self.client.get(reverse('xgds_map_server_feed', args=[feedName]))
+        self.assertEqual(response.status_code, 200)
