286731916dd4a1750bdd86d03cac00c51784d267
Tamar Cohen <tamar.cohen@nasa.gov>
2017-10-12 11:54:54 -0700

    added rest urls

--- xgds_map_server/urls.py
+++ xgds_map_server/urls.py
@@ -14,105 +14,81 @@
 # specific language governing permissions and limitations under the License.
 #__END_LICENSE__
 
-import django.views.static
-from django.conf.urls import url  # pylint: disable=W0401
-from django.contrib.auth.decorators import login_required
+from django.conf.urls import url, include  # pylint: disable=W0401
 
 from django.conf import settings
 from resumable.views import ResumableUploadView
 
 from xgds_map_server import views
 
-urlpatterns = [url(r'^$', views.getMapServerIndexPage,
-                   {},
-                   'xgds_map_server_index'),
-               url(r'^$', views.getMapServerIndexPage,
-                   {},
-                   'map'),
-    url(r'^feedPage/', views.getGoogleEarthFeedPage,
-     {},
-     'xgds_map_server_feed'),
-    # For copying feature data to the session variables
-    url(r'^copyFeatures$', views.copyFeatures, {}, 'copyFeatures'),
-    # for saving map layer itself
-    url(r'^saveMaplayer.json$', views.saveMaplayer, {}, 'saveMaplayer'),
-    # HTML tree of maps
-    url(r'^maptree/', views.getMapTreePage, {},'mapTree'),
-    url(r'^treejson/', views.getFancyTreeJSON, {}, 'mapTreeJSON'),
-    url(r'^selectedjson/', views.getSelectedNodesJSON, {}, 'mapSelectedJSON'),
-    url(r'^uuidsjson/', views.getNodesByUuidJSON, {}, 'mapSelectedUUIDJSON'),
-    url(r'^mapLayerJSON/(?P<layerID>[\w-]+)/', views.getMapLayerJSON, {}, 'mapLayerJSON'),
-    url(r'^setTransparency/(?P<uuid>[\w-]+)/(?P<mapType>\w+)/(?P<value>[\d]+)', views.setTransparency, {}, 'mapSetTransparency'),
+urlpatterns = [url(r'^$', views.getMapServerIndexPage,{},'xgds_map_server_index'),
+               url(r'^$', views.getMapServerIndexPage,{},'map'),
+               url(r'^feedPage/', views.getGoogleEarthFeedPage,{},'xgds_map_server_feed'),
     
-    # Open Map Editor on a map layer
-    url(r'^mapeditor/(?P<layerID>[\w-]+)/', views.getMapEditorPage, {}, 'mapEditLayer'),
-    url(r'^maplayer/kml/(?P<layerID>[\w-]+).kml', views.getMapLayerKML,{},'mapLayerKML'),
-    # HTML detail view of map
-    url(r'^detail/(?P<mapID>[\w-]+)/', views.getMapDetailPage,
-     {},
-     'mapDetail'),
-    # HTML detail of a folder (group)
-    url(r'^folderDetail/(?P<groupID>[\w-]+)/', views.getFolderDetailPage, {}, 'folderDetail'),
-    # HTML view to delete a folder (group)
-    url(r'^folderDelete/(?P<groupID>[\w-]+)/', views.getDeleteFolderPage, {}, 'folderDelete'),
-    # HTML view to add a folder (group)
-    url(r'^folderAdd/', views.getAddFolderPage, {}, 'folderAdd'),
-    # HTML view to add new map
-    url(r'^addkml/', views.getAddKmlPage, {}, 'addKml'),
-    url(r'^addlayer/', views.getAddLayerPage, {}, 'mapAddLayer'),
-    url(r'^addLayerFromSelected/', views.addLayerFromSelected, {}, 'addLayerFromSelected'),
-    url(r'^addTile/', views.getAddTilePage, {}, 'mapAddTile'),
-    url(r'^editTile/(?P<tileID>[\w-]+)/', views.getEditTilePage, {}, 'mapEditTile'),
-    url(r'^addMapDataTile/', views.getAddMapDataTilePage, {}, 'mapAddDataTile'),
-    url(r'^editMapDataTile/(?P<tileID>[\w-]+)/', views.getEditMapDataTilePage, {}, 'mapEditDataTile'),
-    url(r'^addMapSearch/', views.getAddMapSearchPage, {}, 'mapAddMapSearch'),
-    url(r'^editMapSearch/(?P<mapSearchID>[\w-]+)/', views.getEditMapSearchPage, {}, 'mapEditMapSearch'),
-    url(r'^addMapCollection/', views.getAddMapCollectionPage, {}, 'mapAddMapCollection'),
-    url(r'^editMapCollection/(?P<mapCollectionID>[\w-]+)/', views.getEditMapCollectionPage, {}, 'mapEditMapCollection'),
-    url(r'^mapCollectionJSON/(?P<mapCollectionID>[\w-]+)/', views.getMapCollectionJSON, {}, 'mapCollectionJSON'),
-    url(r'^mapSearchJSON/(?P<mapSearchID>[\w-]+)/', views.getMapSearchJSON, {}, 'mapSearchJSON'),
-    url(r'^doMapSearch/', views.searchWithinMap, {}, 'doMapSearch'),
-    url(r'^saveMapSearch/', views.saveSearchWithinMap, {}, 'saveMapSearch'),
-
-    # HTML view to confirm deletion of view
-    url(r'^delete/(?P<nodeID>[\w-]+)/', views.getDeleteNodePage, {}, 'nodeDelete'),
-    # List of deleted maps that can be un-deleted
-    url(r'^deleted/', views.getDeletedNodesPage, {}, 'deletedNodes'),
-    # JSON-accepting url that moves maps/folders around
-    url(r'^moveNode', views.moveNode, {}, 'moveNode'),
-    # JSON-accepting url that changes visibility for a node
-    url(r'^setNodeVisibility', views.setNodeVisibility, {}, 'setNodeVisibility'),
-
-    # --- this url is deprecated, don't use it in new code ---
-    # This URL should receive a static files
-    url(r'^data/(?P<path>.*)$', django.views.static.serve,
-     {'document_root': settings.DATA_URL + settings.XGDS_MAP_SERVER_DATA_SUBDIR,
-      'show_indexes': True,
-      },
-     'xgds_map_server_static'),
-
-    # By default if you just load the app you should see the list
-    url(r'^feed/(?P<feedname>.*)', views.getMapFeed,{},'xgds_map_server_feed'),
-    url(r'^uploadResumable/$', login_required(ResumableUploadView.as_view()), name='uploadResumable'),
-    url(r'^fmapJson/(?P<object_name>[\w]+[\.]*[\w]*)/(?P<filter>[\w]+:[\w]+)$', views.getMappedObjectsJson, {'isLive':settings.GEOCAM_UTIL_LIVE_MODE, 'force':True}, 'xgds_map_server_objectsJson_force'),
-    url(r'^mapJson/(?P<object_name>[\w]+[\.]*[\w]*)/(?P<filter>[\w]+:[\w]+)$', views.getMappedObjectsJson, {'isLive':settings.GEOCAM_UTIL_LIVE_MODE}, 'xgds_map_server_objectsJson'),
-    url(r'^mapJson/(?P<object_name>[\w]+[\.]*[\w]*)/(?P<range>[\d]+)$', views.getMappedObjectsJson, {'isLive':settings.GEOCAM_UTIL_LIVE_MODE}, 'xgds_map_server_objectsJson_range'),
-    url(r'^mapJson/(?P<object_name>[\w]+[\.]*[\w]*)$', views.getMappedObjectsJson, {'range':0, 'force': True, 'isLive':settings.GEOCAM_UTIL_LIVE_MODE}, 'xgds_map_server_objectsJson_default'),
-    url(r'^lastJson/(?P<object_name>[\w]+[\.]*[\w]*)/(?P<filter>[\w]+:[\w]+)$', views.getLastObjectJson, {},'xgds_map_server_lastJson_filter'),
-    url(r'^lastJson/(?P<object_name>[\w]+[\.]*[\w]*)$', views.getLastObjectJson, {}, 'xgds_map_server_lastJson'),
+               # For copying feature data to the session variables
+               url(r'^copyFeatures$', views.copyFeatures, {}, 'copyFeatures'),
     
+               # for saving map layer itself
+               url(r'^saveMaplayer.json$', views.saveMaplayer, {}, 'saveMaplayer'),
     
-    url(r'^search/$', views.getSearchPage, {}, 'search_map'),
-    url(r'^search/(?P<modelName>[\w]+)$', views.getSearchPage, {}, 'search_map_object'),
-    url(r'^search/(?P<modelName>\w+)/(?P<filter>(([\w]+|[a-zA-Z0-9:._\-\s]+),*)+)$', views.getSearchPage, {}, 'search_map_object_filter'),
-    url(r'^view/(?P<modelName>[\w]+)/(?P<modelPK>[\d]+)$', views.getViewSingleModelPage, {}, 'search_map_single_object'),
-    #url(r'^view/(?P<modelName>[\w]+)/(?P<modelPK>[\d]+)$', views.getViewMultiModelPage, {}, 'search_map_single_object'),
-    url(r'^view/(?P<mapName>\w+)/$', views.MapOrderListJson.as_view(), {}, 'map_view_json'),
-    url(r'^view/(?P<mapName>\w+)/(?P<filter>(([\w]+|[a-zA-Z0-9:._\-\s]+),*)+)$', views.MapOrderListJson.as_view(), {}, 'map_view_json_filter'),
+               # HTML tree of maps
+               url(r'^maptree/', views.getMapTreePage, {},'mapTree'),
+               url(r'^setTransparency/(?P<uuid>[\w-]+)/(?P<mapType>\w+)/(?P<value>[\d]+)', views.setTransparency, {}, 'mapSetTransparency'),
+    
+               # Open Map Editor on a map layer
+               url(r'^mapeditor/(?P<layerID>[\w-]+)/', views.getMapEditorPage, {}, 'mapEditLayer'),
 
-    url(r'^json/(?P<mapName>\w+)/(?P<currentPK>[\d]+)$', views.getObject, {}, 'xgds_map_server_object'),
-    url(r'^lastJson2/(?P<mapName>\w+)/$', views.getLastObject, {}, 'xgds_map_server_lastJson2'),
-    url(r'^prevJson/(?P<mapName>\w+)/(?P<currentPK>[\d]+)$', views.getPrevNextObject, {'which':'previous'}, 'xgds_map_server_prevJson'),
-    url(r'^nextJson/(?P<mapName>\w+)/(?P<currentPK>[\d]+)$', views.getPrevNextObject, {'which':'next'}, 'xgds_map_server_nextJson'),
+               # HTML detail view of map
+               url(r'^detail/(?P<mapID>[\w-]+)/', views.getMapDetailPage,{},'mapDetail'),
     
-]
+               # HTML detail of a folder (group)
+               url(r'^folderDetail/(?P<groupID>[\w-]+)/', views.getFolderDetailPage, {}, 'folderDetail'),
+    
+               # HTML view to delete a folder (group)
+               url(r'^folderDelete/(?P<groupID>[\w-]+)/', views.getDeleteFolderPage, {}, 'folderDelete'),
+    
+               # HTML view to add a folder (group)
+               url(r'^folderAdd/', views.getAddFolderPage, {}, 'folderAdd'),
+    
+               # HTML view to add new map
+               url(r'^addkml/', views.getAddKmlPage, {}, 'addKml'),
+               url(r'^addlayer/', views.getAddLayerPage, {}, 'mapAddLayer'),
+               url(r'^addLayerFromSelected/', views.addLayerFromSelected, {}, 'addLayerFromSelected'),
+               url(r'^addTile/', views.getAddTilePage, {}, 'mapAddTile'),
+               url(r'^editTile/(?P<tileID>[\w-]+)/', views.getEditTilePage, {}, 'mapEditTile'),
+               url(r'^addMapDataTile/', views.getAddMapDataTilePage, {}, 'mapAddDataTile'),
+               url(r'^editMapDataTile/(?P<tileID>[\w-]+)/', views.getEditMapDataTilePage, {}, 'mapEditDataTile'),
+               url(r'^addMapSearch/', views.getAddMapSearchPage, {}, 'mapAddMapSearch'),
+               url(r'^editMapSearch/(?P<mapSearchID>[\w-]+)/', views.getEditMapSearchPage, {}, 'mapEditMapSearch'),
+               url(r'^addMapCollection/', views.getAddMapCollectionPage, {}, 'mapAddMapCollection'),
+               url(r'^editMapCollection/(?P<mapCollectionID>[\w-]+)/', views.getEditMapCollectionPage, {}, 'mapEditMapCollection'),
+
+               url(r'^doMapSearch/', views.searchWithinMap, {}, 'doMapSearch'),
+               url(r'^saveMapSearch/', views.saveSearchWithinMap, {}, 'saveMapSearch'),
+
+               # HTML view to confirm deletion of view
+               url(r'^delete/(?P<nodeID>[\w-]+)/', views.getDeleteNodePage, {}, 'nodeDelete'),
+    
+               # List of deleted maps that can be un-deleted
+               url(r'^deleted/', views.getDeletedNodesPage, {}, 'deletedNodes'),
+               
+               # JSON-accepting url that moves maps/folders around
+               url(r'^moveNode', views.moveNode, {}, 'moveNode'),
+               
+               # JSON-accepting url that changes visibility for a node
+               url(r'^setNodeVisibility', views.setNodeVisibility, {}, 'setNodeVisibility'),
+
+               # By default if you just load the app you should see the list
+               url(r'^feed/(?P<feedname>.*)', views.getMapFeed,{},'xgds_map_server_feed'),
+               url(r'^uploadResumable/$', login_required(ResumableUploadView.as_view()), name='uploadResumable'),
+               
+               url(r'^search/$', views.getSearchPage, {}, 'search_map'),
+               url(r'^search/(?P<modelName>[\w]+)$', views.getSearchPage, {}, 'search_map_object'),
+               url(r'^search/(?P<modelName>\w+)/(?P<filter>(([\w]+|[a-zA-Z0-9:._\-\s]+),*)+)$', views.getSearchPage, {}, 'search_map_object_filter'),
+               url(r'^view/(?P<modelName>[\w]+)/(?P<modelPK>[\d]+)$', views.getViewSingleModelPage, {}, 'search_map_single_object'),
+               #url(r'^view/(?P<modelName>[\w]+)/(?P<modelPK>[\d]+)$', views.getViewMultiModelPage, {}, 'search_map_single_object'),
+
+               # Including these in this order ensures that reverse will return the non-rest urls for use in our server
+               url(r'^rest/', include('xgds_map_server.restUrls')),
+               url('', include('xgds_map_server.restUrls')),
+               ]
